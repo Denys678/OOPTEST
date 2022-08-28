98fa32a24e485e663577bebc8cf958da926ec741
Denys Viznyi <denusviznuj@gmail.com>
2022-08-28 12:59:10 +0300

    Create a project

---
+++ slawa morgenshtern/slawa morgenshtern/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  slawa morgenshtern
+//
+//  Created by abb on 28.08.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
